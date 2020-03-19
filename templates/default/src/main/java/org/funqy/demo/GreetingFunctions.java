0fa557676ab55b1c0dab74d6e41dbdf7b7e220b4
Matej Vasek <mvasek@redhat.com>
2020-03-19 16:47:14 +0100

    async example

--- templates/default/src/main/java/org/funqy/demo/GreetingFunctions.java
+++ templates/default/src/main/java/org/funqy/demo/GreetingFunctions.java
@@ -4,6 +4,10 @@ import io.quarkus.funqy.Funq;
 import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 
 public class GreetingFunctions {
     private static final Logger log = Logger.getLogger("funqy.greeting");
@@ -11,14 +15,25 @@ public class GreetingFunctions {
     GreetingService service;
 
     @Funq
-    public Greeting greet(Identity name) {
+    public CompletionStage<Greeting> greet(Identity name) {
         log.info("*** In greeting service ***");
         String message = service.hello(name.getName());
         log.info("Sending back: " + message);
         Greeting greeting = new Greeting();
         greeting.setMessage(message);
         greeting.setName(name.getName());
-        return greeting;
+
+        CompletableFuture<Greeting> result = new CompletableFuture<>();
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                result.complete(greeting);
+            }
+        }, 3000);
+
+        return result;
     }
 
 }
