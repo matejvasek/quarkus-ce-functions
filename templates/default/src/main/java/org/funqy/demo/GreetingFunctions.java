82fdcc1d83cdec163a2b32955447f3fd339a116c
Matej Vasek <mvasek@redhat.com>
2020-04-09 17:21:51 +0200

    feat: support for async functions
    functions may return CompletionStage<>

--- templates/default/src/main/java/org/funqy/demo/GreetingFunctions.java
+++ templates/default/src/main/java/org/funqy/demo/GreetingFunctions.java
@@ -4,21 +4,34 @@ import io.quarkus.funqy.Funq;
 import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
+import java.util.concurrent.*;
 
 public class GreetingFunctions {
     private static final Logger log = Logger.getLogger("funqy.greeting");
+
+    private static final ScheduledExecutorService executor = Executors.newScheduledThreadPool(16);
+
     @Inject
     GreetingService service;
 
     @Funq
-    public Greeting greet(Identity name) {
-        log.info("*** In greeting service ***");
-        String message = service.hello(name.getName());
-        log.info("Sending back: " + message);
-        Greeting greeting = new Greeting();
-        greeting.setMessage(message);
-        greeting.setName(name.getName());
-        return greeting;
+    public CompletionStage<Greeting> greet(Identity name) {
+        CompletableFuture<Greeting> result = new CompletableFuture<>();
+        executor.schedule(()-> {
+            try {
+                log.info("*** In greeting service ***");
+                String message = service.hello(name.getName());
+                log.info("Sending back: " + message);
+                Greeting greeting = new Greeting();
+                greeting.setMessage(message);
+                greeting.setName(name.getName());
+                result.complete(greeting);
+
+            } catch (Throwable t) {
+                result.completeExceptionally(t);
+            }
+        }, 100, TimeUnit.MILLISECONDS);
+        return result;
     }
 
 }
